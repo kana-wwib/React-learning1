a0bb8ed63108a7b91bff20e68a21aeef79a0feb9
kana-wwib <mukrabwy103@gmail.com>
2020-08-20 22:10:40 +0900

    Done 1-8

---
+++ src/Blog.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Article from './Article';
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
+
+const Blog = () => {
+
+
+  // componentDidMount() {
+  //   //ボタンがクリックされたらいいねをカウントアップする
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // } //関数型か関数だけで渡す ()なし
+
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({count: 0})
+  //   }
+  // }
+
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
+
+  // countUp = () => {
+  //   this.setState({count: this.state.count + 1})
+  // }
+
+
+    return (
+      <>
+        <Article 
+        title={"Reactの使い方"} 
+        // count={this.state.count} 
+        />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+      </>
+    )
+}
+
+export default Blog
